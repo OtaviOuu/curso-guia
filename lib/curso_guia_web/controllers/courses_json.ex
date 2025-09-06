7526888188f69a993e2d0d9658e7ac50697f300c
otaviouu <otavio.venancio.oliveira@usp.br>
2025-09-06 14:59:48 -0300

    refactor(courses_json): update course data mapping functions for consistency

--- lib/curso_guia_web/controllers/courses_json.ex
+++ lib/curso_guia_web/controllers/courses_json.ex
@@ -1,21 +1,31 @@
 defmodule CursoGuiaWeb.CoursesJSON do
   def index(%{courses: courses}) do
-    %{data: Enum.map(courses, &course_to_json_data/1)}
+    %{data: Enum.map(courses, &course_detailed_data/1)}
   end
 
   def show(%{course: course}) do
-    %{data: course_to_json_data(course)}
+    %{data: course_summary_data(course)}
   end
 
-  defp course_to_json_data(course) do
+  defp course_detailed_data(course) do
     %{
       id: course.id,
       title: course.title,
-      description: course.description,
       cover: course.cover,
+      description: course.description,
       href: course.href,
       rating: course.rating,
       views: course.views
+    }
+  end
+
+  defp course_summary_data(course) do
+    %{
+      id: course.id,
+      title: course.title,
+      cover: course.cover,
+      description: course.description,
+      href: course.href
     }
   end
 end
